92849f0b4bd07996a32442003a1ba91a4ce86e6b
Christopher Stanley <chris@cbstanley.dev>
2020-07-17 01:27:50 -0600

    Display public stories

--- app.js
+++ app.js
@@ -29,10 +29,21 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate } = require('./helpers/hbs');
+const { formatDate, truncate, stripTags } = require('./helpers/hbs');
 
 // Handlebars
-app.engine('.hbs', exphbs({ helpers: {formatDate}, defaultLayout: 'main', extname: '.hbs'}));
+app.engine(
+    '.hbs',
+    exphbs({
+        helpers: {
+            formatDate,
+            truncate,
+            stripTags,
+        },
+        defaultLayout: 'main',
+        extname: '.hbs'
+    })
+);
 app.set('view engine', '.hbs');
 
 // Session Middleware
